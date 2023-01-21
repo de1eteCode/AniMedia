32027819bafe397d2c211e117de7a6dbcdc1100c
de1eteCode <skozlov2015@yandex.ru>
2023-01-21 19:40:06 +0500

    Фикс нормализации дефолтных значений в БД

--- AniMedia.Identity/Configurations/RoleConfiguration.cs
+++ AniMedia.Identity/Configurations/RoleConfiguration.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AniMedia.Identity.Configurations;
 
-internal class RoleConfiguration : IEntityTypeConfiguration<IdentityRole<Guid>> {
+internal class RoleConfiguration : BaseEntityTypeConfiguration<IdentityRole<Guid>> {
     internal static readonly StaticRoleKeyNamePair ADMIN = new(Guid.Parse("C4E2F893-6C52-487F-9553-9306395F282F"), "Администратор");
     internal static readonly StaticRoleKeyNamePair USER = new(Guid.Parse("8238CA98-C992-46D8-9CBA-90AF4425BE5B"), "Пользователь");
 
-    private readonly ILookupNormalizer _lookupNormalizer = new UpperInvariantLookupNormalizer();
-
-    public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder) {
+    public override void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder) {
         builder.HasData(
             new IdentityRole<Guid> {
                 Id = ADMIN.UID,
--- AniMedia.Identity/Configurations/UserConfiguration.cs
+++ AniMedia.Identity/Configurations/UserConfiguration.cs
@@ -1,24 +1,23 @@
 ﻿using AniMedia.Identity.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AniMedia.Identity.Configurations;
 
-internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser> {
+internal class UserConfiguration : BaseEntityTypeConfiguration<ApplicationUser> {
     internal static readonly Guid ADMIN_UID = Guid.Parse("A6330A68-A4E3-466C-9A03-6B45C093F90F");
 
-    public void Configure(EntityTypeBuilder<ApplicationUser> builder) {
+    public override void Configure(EntityTypeBuilder<ApplicationUser> builder) {
         var hasher = new PasswordHasher<ApplicationUser>();
 
         builder.HasData(
             new ApplicationUser() {
                 Id = ADMIN_UID,
                 Email = "admin@localhost.com",
-                NormalizedEmail = "admin@localhost.com".Normalize(),
+                NormalizedEmail = _lookupNormalizer.NormalizeEmail("admin@localhost.com"),
                 EmailConfirmed = true,
                 UserName = "admin",
-                NormalizedUserName = "admin".Normalize(),
+                NormalizedUserName = _lookupNormalizer.NormalizeName("admin"),
                 FirstName = "Admin",
                 SecondName = "",
                 PasswordHash = hasher.HashPassword(null!, "P@ssw0rd")
