87c000a43dffc716c9a38e9363c1878cf659c192
de1eteCode <skozlov2015@yandex.ru>
2023-04-23 19:40:46 +0500

    Кривая верстка под админку

--- AniMedia.WebClient/Constants/PageUrlConstants.cs
+++ AniMedia.WebClient/Constants/PageUrlConstants.cs
@@ -1,22 +1,17 @@
 ﻿namespace AniMedia.WebClient.Constants;
 
 public static class PageUrlConstants {
-
-    #region Account
+    public const string PrefixAdminPanel = "/admin";
 
     public const string LoginPageUrl = "/account/login";
     public const string RegisterPageUrl = "/account/register";
     public const string ProfilePageUrl = "/account/profile";
 
-    #endregion Account
-
-    #region ApiUI
-
     public const string SwaggerPageUrl = "/swagger";
 
-    #endregion ApiUI
-
     public const string IndexPageUrl = "/";
+    public const string AdminIndexPageUrl = PrefixAdminPanel + IndexPageUrl;
 
     public const string CounterPageUrl = "/counter";
+
 }
