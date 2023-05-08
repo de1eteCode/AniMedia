5512e4a814db60b2efe815a94282045e62cdca97
de1eteCode <skozlov2015@yandex.ru>
2023-05-08 21:46:10 +0500

    Верстка и доп страницы под реализацию

--- AniMedia.WebClient/Constants/PageUrlConstants.cs
+++ AniMedia.WebClient/Constants/PageUrlConstants.cs
@@ -6,12 +6,12 @@ public static class PageUrlConstants {
     public const string LoginPageUrl = "/account/login";
     public const string RegisterPageUrl = "/account/register";
     public const string ProfilePageUrl = "/account/profile";
-
-    public const string SwaggerPageUrl = "/swagger";
-
     public const string IndexPageUrl = "/";
-    public const string AdminIndexPageUrl = PrefixAdminPanel + IndexPageUrl;
-
     public const string CounterPageUrl = "/counter";
 
+    public const string AdminIndexPageUrl = PrefixAdminPanel + IndexPageUrl;
+    public const string SwaggerPageUrl = PrefixAdminPanel + "/swagger";
+    public const string ManageUsersPageUrl = PrefixAdminPanel + "/users";
+    public const string ManageAnimeSeriesPageUrl = PrefixAdminPanel + "/animeseries";
+    public const string ManageRulesPageUrl = PrefixAdminPanel + "/rules";
 }
---
+++ AniMedia.WebClient/UIWeb/AdminPanel/Pages/ManageUsersPage.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AniMedia.WebClient.UIWeb.AdminPanel.Pages; 
+
+public partial class ManageUsersPage : ComponentBase {
+    
+}
